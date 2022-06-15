8e603b1d07ee8486cf0e93ed90cb2902b8aa4f96
TejaReddy255 <39689987+TejaReddy255@users.noreply.github.com>
2022-06-15 19:22:44 +0530

    created getusers

--- cpr/getUsers.py
+++ cpr/getUsers.py
@@ -1,4 +1,45 @@
-
+from cpr.models import Tech,User
 
 def getUsers(tech):
-    return "user"
+    
+    l=[]
+    d={}
+    if(Tech.query.filter(Tech.tech1 == tech).all() != None):
+        for i in Tech.query.filter(Tech.tech1 == tech).all():
+            l.append(User.query.filter_by(id=i.user_id).first())
+    if(Tech.query.filter(Tech.tech2 == tech).all() != None):
+        for i in Tech.query.filter(Tech.tech2 == tech).all():
+            l.append(User.query.filter_by(id=i.user_id).first())
+    if(Tech.query.filter(Tech.tech3 == tech).all() != None):
+        for i in Tech.query.filter(Tech.tech3 == tech).all():
+            l.append(User.query.filter_by(id=i.user_id).first())
+    if(Tech.query.filter(Tech.tech4 == tech).all() != None):
+        for i in Tech.query.filter(Tech.tech4 == tech).all():
+            l.append(User.query.filter_by(id=i.user_id).first())
+    if(Tech.query.filter(Tech.tech5 == tech).all() != None):
+        for i in Tech.query.filter(Tech.tech5 == tech).all():
+            l.append(User.query.filter_by(id=i.user_id).first())
+    if(Tech.query.filter(Tech.tech6 == tech).all() != None):
+        for i in Tech.query.filter(Tech.tech6 == tech).all():
+            l.append(User.query.filter_by(id=i.user_id).first())
+    if(Tech.query.filter(Tech.tech7 == tech).all() != None):
+        for i in Tech.query.filter(Tech.tech7 == tech).all():
+            l.append(User.query.filter_by(id=i.user_id).first())
+    if(Tech.query.filter(Tech.tech8 == tech).all() != None):
+        for i in Tech.query.filter(Tech.tech8 == tech).all():
+            l.append(User.query.filter_by(id=i.user_id).first())
+    if (Tech.query.filter(Tech.tech9 == tech).all() != None):
+        for i in Tech.query.filter(Tech.tech9 == tech).all():
+            l.append(User.query.filter_by(id=i.user_id).first())
+    if (Tech.query.filter(Tech.tech10 == tech).all() != None):
+        for i in Tech.query.filter(Tech.tech10 == tech).all():
+            l.append(User.query.filter_by(id=i.user_id).first())
+    if l==[]:
+        return []
+    else :
+        for i in l:
+             d[i.email] = i.username
+
+
+    return d  
+
